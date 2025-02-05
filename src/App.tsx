2352e582e3115f917a5e30ba8dcd7008818c2f11
Marc Seiferle <marc.seiferle@bbcag.ch>
2025-02-05 19:41:22 +0100

    put peer dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { RetroDigit } from 'react-retro-digit'
 
 function App() {
   return (
-    <div style={{ display: 'flex', gap: 2 }}>
-      <RetroDigit color="indigo" alphaRatio="0.05" number={7} size="1.5rem" />
+    <div
+      style={{
+        display: 'flex',
+        gap: 2,
+        height: '20vh',
+        width: '100vw',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <RetroDigit color="indigo" alphaRatio="0.05" number={7} size="75%" />
+      <RetroDigit color="indigo" alphaRatio="0.05" number={5} size="75%" />
     </div>
   )
 }
